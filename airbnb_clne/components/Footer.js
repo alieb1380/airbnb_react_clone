41b18f371d517d5c3d76ff62e3c4480c4e3aea99
Ali <alieb1380alieb@gmail.com>
2021-08-07 08:00:23 +0430

    Day_One

---
+++ airbnb_clne/components/Footer.js
@@ -0,0 +1,39 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">ABOUT</h5>
+                <p>How Airbnb works</p>
+                <p>Newsroom</p>
+                <p>Inventors</p>
+                <p>Airbnb Plus</p>
+                <p>Airbnb luxe</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">COMMUNITY</h5>
+                <p>Accessibility</p>
+                <p>This is Not A Real Site</p>
+                <p>It's a pretty awsome clone</p>
+                <p>Referrals accepted</p>
+                <p>IG=== @Ali__eb</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">HOST</h5>
+                <p>Ali React Next.Js</p>
+                <p>Hundreds of Students</p>
+                <p>Join Now!</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+                <h5 className="font-bold">SUPPORT</h5>
+                <p>Help Center</p>
+                <p>Trust & Safety</p>
+                <p>For win</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
